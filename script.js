a22b069d009865dec782cde003ca180b99abf6fc
seikhchilli <saurabhkrsingh2001@gmail.com>
2021-07-08 12:30:48 +0530

    complete

--- script.js
+++ script.js
@@ -1,26 +1,63 @@
-var displaycounter = document.getElementById("display");
+var flipper = document.getElementById("flipcard");
+y = false;
+t = false;
+var rotation;
 var counter = 0;
 
-var flipper = document.getElementById("flipcard");
-
 var x = document.getElementById("increment").addEventListener("click", function () {
-    counter++;
-    var rotation = 180 * counter;
-    flipper.style = "transform: rotateY(" + rotation + "deg)";
-    displaycounter.innerHTML = counter;
-        //document.getElementById("decrement").id = "wait";
-        //document.getElementById("increment").id = "decrement";
-        //document.getElementById("wait").id = "increment";
+    if (y) {
+        counter++;
+        setTimeout(function () {
+            document.getElementById("decrement").innerHTML = "+";
+            document.getElementById("increment").innerHTML = "-";
+            document.getElementById("display").style = "transform: rotateY(-" + rotation + "deg)";
+            document.getElementById("display").innerHTML = counter;
+        }, 1200);
 
+
+    }
+    else {
+        counter--;
+        setTimeout(function () {
+            document.getElementById("decrement").innerHTML = "-";
+            document.getElementById("increment").innerHTML = "+";
+            document.getElementById("display").style = "transform: rotateY(-" + rotation + "deg)";
+            document.getElementById("display").innerHTML = counter;
+        }, 1200);
+
+    }
+    rotation = counter * 180;
+    
+    flipper.style = "transform: rotateY(" + rotation + "deg)";
+    
 })
 
-var y = document.getElementById("decrement").addEventListener("click", function () {
-    counter--;
-    var rotation = 180 * counter;
-    flipper.style = "transform: rotateY(" + rotation + "deg)";
-    displaycounter.innerHTML = counter;
-    //document.getElementById("decrement").id = "wait";
-    //document.getElementById("increment").id = "decrement";
-    //document.getElementById("wait").id = "increment";
+var x = document.getElementById("decrement").addEventListener("click", function () {
+    if (t) {
+        counter--;
+        setTimeout(function () {
+            document.getElementById("decrement").innerHTML = "+";
+            document.getElementById("increment").innerHTML = "-";
+            document.getElementById("display").style = "transform: rotateY(-" + rotation + "deg)";
+            document.getElementById("display").innerHTML = counter;
+        }, 1200);
 
+    }
+    else {
+        counter++;
+        setTimeout(function () {
+            document.getElementById("decrement").innerHTML = "-";
+            document.getElementById("increment").innerHTML = "+";
+            document.getElementById("display").style = "transform: rotateY(-" + rotation + "deg)";
+            document.getElementById("display").innerHTML = counter;
+        }, 1200);
+
+
+    }
+    rotation = counter * 180;
+
+    flipper.style = "transform: rotateY(" + rotation + "deg)";
+   
+    
+    
 })
